2abe4225d30792231c2d655577d15b743a8d8f26
krydenk93 <krydenk@gmail.com>
2020-03-25 11:28:48 +0700

    Add TimeFieldView + some compatibility fixes

--- form/DateField/DateFieldView.js
+++ form/DateField/DateFieldView.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 
 import {bem} from '@steroidsjs/core/hoc';
+import MomentLocaleUtils from "react-day-picker/moment";
 
 @bem('DateFieldView')
 export default class DateFieldView extends React.PureComponent {
@@ -27,6 +28,17 @@ export default class DateFieldView extends React.PureComponent {
             <div>
                 <DayPickerInput
                     {...this.props.pickerProps}
+                    name={this.props.input.name}
+                    placeholder={this.props.placeholder || this.props.displayFormat}
+                    value={this.props.parseDate(this.props.input.value)}
+                    parseDate={this.props.parseDate}
+                    formatDate={this.props.formatDate}
+                    onDayChange={(value) => this.props.onChange(value)}
+                    dayPickerProps={{
+                        locale: this.props.locale.language,
+                        localeUtils: this.props.localeUtils,
+                        ...(this.props.pickerProps && this.props.pickerProps.dayPickerProps)
+                    }}
                     inputProps={{
                         ...(this.props.pickerProps && this.props.pickerProps.inputProps),
                         className: bem(
