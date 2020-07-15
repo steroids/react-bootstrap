55d3f12cea3b9eea5f302826ee7051d622d55a1a
Vladimir Kozhin <hello@kozhindev.com>
2020-07-15 15:02:24 +0700

    v2.0.0-beta.10 rename IconView -> Icon folder

---
+++ index.ts
@@ -0,0 +1,105 @@
+import ControlsView from './crud/Controls/ControlsView';
+import CrudView from './crud/Crud/CrudView';
+import AutoCompleteFieldView from './form/AutoCompleteField/AutoCompleteFieldView';
+import ButtonView from './form/Button/ButtonView';
+import CheckboxFieldView from './form/CheckboxField/CheckboxFieldView';
+import CheckboxListFieldView from './form/CheckboxListField/CheckboxListFieldView';
+import DateFieldView from './form/DateField/DateFieldView';
+import DateTimeFieldView from './form/DateTimeField/DateTimeFieldView';
+import DropDownFieldView from './form/DropDownField/DropDownFieldView';
+import FieldLayoutView from './form/FieldLayout/FieldLayoutView';
+import FieldListView from './form/FieldList/FieldListView';
+import FieldListItemView from './form/FieldList/FieldListItemView';
+import FileFieldView from './form/FileField/FileFieldView';
+import FileFieldItemView from './form/FileField/FileFieldItemView';
+import FormView from './form/Form/FormView';
+import HtmlFieldView from './form/HtmlField/HtmlFieldView';
+import InputFieldView from './form/InputField/InputFieldView';
+import NumberFieldView from './form/NumberField/NumberFieldView';
+import PasswordFieldView from './form/PasswordField/PasswordFieldView';
+import RadioListFieldView from './form/RadioListField/RadioListFieldView';
+import RangeFieldView from './form/RangeField/RangeFieldView';
+import ReCaptchaFieldView from './form/ReCaptchaField/ReCaptchaFieldView';
+import SliderFieldView from './form/SliderField/SliderFieldView';
+import SwitcherFieldView from './form/SwitcherField/SwitcherFieldView';
+import TextFieldView from './form/TextField/TextFieldView';
+import TimeFieldView from './form/TimeField/TimeFieldView';
+import IconView from './icon/Icon/IconView';
+import HeaderView from './layout/Header/HeaderView';
+import LoaderView from './layout/Loader/LoaderView';
+import NotificationsItemView from './layout/Notifications/NotificationsItemView';
+import NotificationsView from './layout/Notifications/NotificationsView';
+import AccordionView from './list/Accordion/AccordionView';
+import ActionColumnView from './list/ActionColumn/ActionColumnView';
+import CheckboxColumnView from './list/CheckboxColumn/CheckboxColumnView';
+import DetailView from './list/Detail/DetailView';
+import EmptyView from './list/Empty/EmptyView';
+import GridView from './list/Grid/GridView';
+import ListView from './list/List/ListView';
+import ListItemView from './list/List/ListItemView';
+import PaginationMoreView from './list/Pagination/PaginationMoreView';
+import PaginationButtonView from './list/Pagination/PaginationButtonView';
+import PaginationSizeView from './list/PaginationSize/PaginationSizeView';
+import ModalView from './modal/Modal/ModalView';
+import BreadcrumbsView from './nav/Breadcrubms/BreadcrumbsView';
+import NavBarView from './nav/Nav/NavBarView';
+import NavButtonView from './nav/Nav/NavButtonView';
+import NavIconView from './nav/Nav/NavIconView';
+import NavLinkView from './nav/Nav/NavLinkView';
+import NavListView from './nav/Nav/NavListView';
+import NavTabsView from './nav/Nav/NavTabsView';
+import TreeView from './nav/Tree/TreeView';
+
+export default {
+    'crud.ControlsView': ControlsView,
+    'crud.CrudView': CrudView,
+    'form.AutoCompleteFieldView': AutoCompleteFieldView,
+    'form.ButtonView': ButtonView,
+    'form.CheckboxFieldView': CheckboxFieldView,
+    'form.CheckboxListFieldView': CheckboxListFieldView,
+    'form.DateFieldView': DateFieldView,
+    'form.DateTimeFieldView': DateTimeFieldView,
+    'form.DropDownFieldView': DropDownFieldView,
+    'form.FieldLayoutView': FieldLayoutView,
+    'form.FieldListView': FieldListView,
+    'form.FieldListItemView': FieldListItemView,
+    'form.FileFieldView': FileFieldView,
+    'form.FileFieldItemView': FileFieldItemView,
+    'form.FormView': FormView,
+    'form.HtmlFieldView': HtmlFieldView,
+    'form.InputFieldView': InputFieldView,
+    'form.NumberFieldView': NumberFieldView,
+    'form.PasswordFieldView': PasswordFieldView,
+    'form.RadioListFieldView': RadioListFieldView,
+    'form.RangeFieldView': RangeFieldView,
+    'form.ReCaptchaFieldView': ReCaptchaFieldView,
+    'form.SliderFieldView': SliderFieldView,
+    'form.SwitcherFieldView': SwitcherFieldView,
+    'form.TextFieldView': TextFieldView,
+    'form.TimeFieldView': TimeFieldView,
+    'icon.IconView': IconView,
+    'layout.HeaderView': HeaderView,
+    'layout.LoaderView': LoaderView,
+    'layout.NotificationsView': NotificationsView,
+    'layout.NotificationsItemView': NotificationsItemView,
+    'list.AccordionView': AccordionView,
+    'list.ActionColumnView': ActionColumnView,
+    'list.CheckboxColumnView': CheckboxColumnView,
+    'list.DetailView': DetailView,
+    'list.EmptyView': EmptyView,
+    'list.GridView': GridView,
+    'list.ListView': ListView,
+    'list.ListItemView': ListItemView,
+    'list.PaginationButtonView': PaginationButtonView,
+    'list.PaginationMoreView': PaginationMoreView,
+    'list.PaginationSizeView': PaginationSizeView,
+    'modal.ModalView': ModalView,
+    'nav.BreadcrumbsView': BreadcrumbsView,
+    'nav.NavBarView': NavBarView,
+    'nav.NavButtonView': NavButtonView,
+    'nav.NavIconView': NavIconView,
+    'nav.NavLinkView': NavLinkView,
+    'nav.NavListView': NavListView,
+    'nav.NavTabsView': NavTabsView,
+    'nav.TreeView': TreeView,
+};
