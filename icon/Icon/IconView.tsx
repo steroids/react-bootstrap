359b12b3f0112e5c3da31ef07e237cbba9688aa2
krydenk93 <krydenk@gmail.com>
2020-08-04 14:03:02 +0700

    Move web specific code to appropriate component

--- icon/Icon/IconView.tsx
+++ icon/Icon/IconView.tsx
@@ -4,6 +4,7 @@ import './IconView.scss';
 import {bem} from '@steroidsjs/core/hoc';
 import {IBemHocOutput} from '@steroidsjs/core/hoc/bem';
 import {IIconViewProps} from '@steroidsjs/core/ui/icon/Icon/Icon';
+import _isString from 'lodash-es/isString';
 
 interface IIconViewInnerProps extends IIconViewProps, IBemHocOutput {
     className: string,
@@ -15,10 +16,19 @@ export default class IconView extends React.PureComponent<IIconViewInnerProps> {
 
     render() {
         const bem = this.props.bem;
+
+        let icon = this.props.icon;
+        if (_isString(this.props.icon)) {
+            // @ts-ignore
+            icon = !this.props.icon.match(/<svg/)
+                ? `<img alt=${this.props.name} src=${icon} />`
+                : icon;
+        }
+
         return (
             <span
                 className={bem(bem.block(), this.props.className)}
-                dangerouslySetInnerHTML={{__html: this.props.icon as string}}
+                dangerouslySetInnerHTML={{__html: icon as string}}
                 title={this.props.title}
             />
         );
