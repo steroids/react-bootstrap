64143d269bd0305568f51e8eea67aa38cd097c0b
kahegal <kahegal89@gmail.com>
2020-08-27 10:17:08 +0700

    Modal - fixed centered position

--- modal/Modal/ModalView.tsx
+++ modal/Modal/ModalView.tsx
@@ -27,32 +27,30 @@ export default class ModalView extends React.PureComponent<IModalViewProps & IBe
                     bodyOpenClassName={bem.block('body-opened')}
                     ariaHideApp={false}
                 >
-                    <div className={bem.element('inner')}>
-                        <div className={bem.element('header')}>
-                            <span className={bem.element('title')}>
-                                {this.props.title}
-                            </span>
-                            <a
-                                className={bem.element('close')}
-                                href='#'
-                                onClick={e => {
-                                    e.preventDefault();
-                                    this.props.onClose();
-                                }}
+                    <div className={bem.element('header')}>
+                        <span className={bem.element('title')}>
+                            {this.props.title}
+                        </span>
+                        <a
+                            className={bem.element('close')}
+                            href='#'
+                            onClick={e => {
+                                e.preventDefault();
+                                this.props.onClose();
+                            }}
+                        />
+                    </div>
+                    <div className={bem.element('content')}>
+                        {this.props.children}
+                    </div>
+                    {this.props.controls && (
+                        <div className={bem.element('footer')}>
+                            <Controls
+                                items={this.props.controls}
+                                className={bem.element('controls')}
                             />
                         </div>
-                        <div className={bem.element('content')}>
-                            {this.props.children}
-                        </div>
-                        {this.props.controls && (
-                            <div className={bem.element('footer')}>
-                                <Controls
-                                    items={this.props.controls}
-                                    className={bem.element('controls')}
-                                />
-                            </div>
-                        )}
-                    </div>
+                    )}
                 </Modal>
             </div>
         );
