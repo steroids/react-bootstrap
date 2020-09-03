7197af21519709485e5a41ae7f5f55fd7bc8d845
stiivenson <nicklihorus4@gmail.com>
2020-09-03 17:28:14 +0700

    add TooltipView

---
+++ layout/Tooltip/TooltipView.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import {bem} from '@steroidsjs/core/hoc';
+import {ITooltipProps} from '@steroidsjs/core/ui/layout/Tooltip/Tooltip';
+import {IBemHocOutput} from '@steroidsjs/core/hoc/bem';
+
+import './TooltipView.scss';
+
+@bem('Tooltip')
+export default class TooltipView extends React.PureComponent<ITooltipProps & IBemHocOutput> {
+
+    tooltipRef: React.RefObject<any>
+
+    constructor(props) {
+        super(props);
+        this.tooltipRef = React.createRef();
+    }
+
+    componentDidMount(): void {
+        this.props.calculatePosition(this.tooltipRef.current.getBoundingClientRect());
+    }
+
+    render() {
+        const bem = this.props.bem;
+        return (
+            <div
+                ref={this.tooltipRef}
+                className={bem.block()}
+                style={this.props.style}
+            >
+                <div
+                    className={bem.element('body',
+                        {
+                            ['position-'+this.props.position]: true,
+                            'show': this.props.isTooltipVisible
+                        }
+                    )}
+                >
+                    <div
+                        className={bem.element(
+                            'arrow',
+                            {['position-'+this.props.position]: true},
+                        )}
+                        style={this.props.arrowPosition}
+                    />
+                    <div className={bem.element('content')}>
+                        <span>{this.props.content}</span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
