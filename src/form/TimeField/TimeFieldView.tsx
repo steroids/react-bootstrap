a056a19b638135c0686f75e6f9858e3848746b4c
nlichodeevskiy <nicklihorus4@gmail.com>
2021-04-13 17:18:41 +0700

    upd TimeField

--- src/form/TimeField/TimeFieldView.tsx
+++ src/form/TimeField/TimeFieldView.tsx
@@ -1,98 +1,69 @@
 import * as React from 'react';
-import {IBemHocOutput} from '@steroidsjs/core/hoc/bem';
-import {ITimeFieldViewProps} from '@steroidsjs/core/ui/form/TimeField/TimeField';
-import {useBem} from '@steroidsjs/core/hooks';
-import {useMemo} from 'react';
-import _padStart from 'lodash-es/padStart';
-import Icon from '@steroidsjs/core/ui/icon/Icon';
+import {IBemHocOutput} from '../../../../react/src/hoc/bem';
+import {ITimeFieldViewProps} from '../../../../react/src/ui/form/TimeField/TimeField';
+import {useBem} from '../../../../react/src/hooks';
+
+import TimePanelView from './TimePanelView';
+import Icon from '../../../../react/src/ui/icon/Icon';
 
 import './TimeFieldView.scss';
 
 
 export default function TimeFieldView(props: ITimeFieldViewProps & IBemHocOutput) {
     const bem = useBem('TimeFieldView');
-    const hours = useMemo(() => {
-        const result = [];
-        for (let i = 0; i < 24; i++) {
-            const hour = _padStart(i, 2, '0');
-            result.push(hour);
-        }
-        return result
-    }, []);
-    const minutes = useMemo(() => {
-        const result = [];
-        for (let i = 0; i < 60; i++) {
-            const minute = _padStart(i, 2, '0');
-            result.push(minute);
-        }
-        return result
-    }, []);
     return (
         <div
+            className={bem(
+                bem.block({
+                    'disabled': props.disabled,
+                    'no-border': props.noBorder,
+                }), props.className
+            )}
             ref={props.forwardedRef}
-            className={bem(bem.block(), props.className)}
             onFocus={(e) => {
                 e.preventDefault();
                 props.openDropDown();
             }}
+            onBlur={(e) => {
+                e.preventDefault();
+                props.onBlur();
+            }}
         >
-            <input
-                {...props.inputProps}
-                className={bem(
-                    bem.element('input', {
-                        size: props.size,
-                    }),
-                    props.isInvalid && 'is-invalid',
-                )}
-                autoComplete='off'
-                disabled={props.disabled}
-                placeholder={props.placeholder}
-                required={props.required}
-                type={props.type}
-                onChange={e => props.inputProps.onChange(e.target.value)}
-            />
-            <Icon
-                className={bem.element('icon')}
-                name='clock'
-            />
-            {props.showDropDown && (
-                <div className={bem.element('time-panel')}>
-                    <div className={bem.element('time-panel-body')}>
-                        <ul className={bem.element('time-panel-column')}>
-                            {hours.map((value, index) => (
-                                <li
-                                    key={index}
-                                    className={bem.element('time-panel-cell')}
-                                    onClick={e => props.changeHours(value)}
-                                >
-                                    <div className={bem.element('time-panel-column-cell-value')}>
-                                        {value}
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                        <ul
-                            className={bem.element('time-panel-column')}
-                        >
-                            {minutes.map((value, index) => (
-                                <li
-                                    key={index}
-                                    className={bem.element('time-panel-cell')}
-                                    onClick={(e) => props.changeMinutes(value)}
-                                >
-                                    <div className={bem.element('time-panel-column-cell-value')}>
-                                        {value}
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className={bem.element('time-panel-footer')}>
-                        <button>
-                            Ok
-                        </button>
-                    </div>
+            <div className={bem.element('body')}>
+                <input
+                    {...props.inputProps}
+                    className={bem(
+                        bem.element('input', {
+                            size: props.size,
+                        }),
+                        props.isInvalid && 'is-invalid',
+                    )}
+                    autoComplete='off'
+                    disabled={props.disabled}
+                    placeholder={props.placeholder}
+                    required={props.required}
+                    type={props.type}
+                    onChange={e => props.inputProps.onChange(e.target.value)}
+                />
+                <div className={bem.element('icon-container')}>
+                    <Icon
+                        className={bem.element('icon')}
+                        name='clock'
+                    />
+                    {props.showRemove && props.input.value && (
+                        <Icon
+                            className={bem.element('icon', 'close')}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                props.clearInput();
+                            }}
+                            name='times-circle'
+                        />
+                    )}
                 </div>
+            </div>
+            {props.showDropDown && (
+                <TimePanelView {...props} />
             )}
         </div>
     );
---
+++ src/form/TimeField/TimePanelView.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import {IBemHocOutput} from '../../../../react/src/hoc/bem';
+import {ITimeFieldViewProps} from '../../../../react/src/ui/form/TimeField/TimeField';
+import {useBem} from '../../../../react/src/hooks';
+import {useMemo} from 'react';
+import _padStart from 'lodash-es/padStart';
+
+import './TimePanelView.scss';
+
+
+export default function TimePanelView(props: ITimeFieldViewProps & IBemHocOutput) {
+    const bem = useBem('TimePanelView');
+    const hours = useMemo(() => {
+        const result = [];
+        for (let i = 0; i < 24; i++) {
+            const hour = _padStart(i, 2, '0');
+            result.push(hour);
+        }
+        return result
+    }, []);
+    const minutes = useMemo(() => {
+        const result = [];
+        for (let i = 0; i < 60; i++) {
+            const minute = _padStart(i, 2, '0');
+            result.push(minute);
+        }
+        return result
+    }, []);
+    return (
+        <div className={bem.block()}>
+            <div className={bem.element('body')}>
+                <ul className={bem.element('column')}>
+                    {hours.map((value, index) => (
+                        <li
+                            key={index}
+                            className={bem.element('cell', {
+                                'selected': value === (props.input.value?.substring(0, 2) || '00')
+                            })}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                props.changeHours(value);
+                            }}
+                        >
+                            <div className={bem.element('cell-value')}>
+                                {value}
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <ul className={bem.element('column')}>
+                    {minutes.map((value, index) => (
+                        <li
+                            key={index}
+                            className={bem.element('cell', {
+                                'selected': value === (props.input.value?.substring(3, 6) || '00')
+                            })}
+                            onClick={(e) => props.changeMinutes(value)}
+                        >
+                            <div className={bem.element('cell-value')}>
+                                {value}
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className={bem.element('footer')}>
+                <button
+                    className={bem.element('button', 'now')}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        props.setNow();
+                    }}
+                >
+                    Now
+                </button>
+                {/*// TODO Add OK logic*/}
+                <button className={bem.element('button', 'ok')}>
+                    Ok
+                </button>
+            </div>
+        </div>
+    );
+}
