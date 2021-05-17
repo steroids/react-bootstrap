f78f925086b4aaf1ab8cbc94862321519e61d4ce
nlichodeevskiy <nicklihorus4@gmail.com>
2021-05-17 14:30:26 +0700

    refactoring Avatar

--- src/content/Avatar/AvatarView.tsx
+++ src/content/Avatar/AvatarView.tsx
@@ -1,13 +1,13 @@
-import React, {useState, isValidElement} from 'react';
+import React, {useState} from 'react';
 import { useBem } from '@steroidsjs/core/hooks';
 import { IAvatarViewProps } from '@steroidsjs/core/ui/content/Avatar/Avatar'
 
 import './AvatarView.scss';
 
+
 export default function Avatar (props: IAvatarViewProps) {
     const bem = useBem('AvatarView');
-
-    const [isImgExist, setIsImgExist] = useState(true);
+    const [isError, setIsError] = useState<boolean>(false);
 
     const customSize: React.CSSProperties =
         typeof props.size === 'number'
@@ -15,45 +15,34 @@ export default function Avatar (props: IAvatarViewProps) {
             width: props.size,
             height: props.size,
             lineHeight: `${props.size}px`,
-            fontSize:  props.size / 3,
+            fontSize:  props.size / 2,
             }
         : {};
 
-    const hasImageElement = isValidElement(props.src);
-
-    let renderingChildren;
-    if (typeof props.src === 'string' && isImgExist) {
-        renderingChildren = (
-            <img src={props.src} srcSet={props.srcSet} alt={props.alt}/>
-        );
-    } else if (hasImageElement) {
-        renderingChildren = props.src;
-    } else if (props.children) {
-        renderingChildren = (
-            <span>{props.children}</span>
-        )
-    } else if (typeof props.alt === 'string') {
-        renderingChildren = (
-            <span>{props.alt}</span>
-        )
-    }  else if (props.src && props.children === "") {
-        renderingChildren = (
-            <img src="https://hh.ru/employer-logo/3775460.png" srcSet={props.srcSet}/>
-        )
-    }
-
-    return(
+    return (
         <span 
             className={bem.block({
-                [`large`]: props.size === 'large',
-                [`medium`]: props.size === 'medium',
-                [`small`]: props.size === 'small',
-                [`square`]: props.shape === 'square',
-                [`circle`]: props.shape === 'circle',
+                [`${props.size}`]: typeof props.size === 'string',
+                [`${props.shape}`]: !!props.shape,
+                'has-image': !!props.src && !isError,
             })}
-            style={ {...customSize}} 
+            style={{...customSize}}
         >
-            {renderingChildren}
+            {props.src && (isError && (
+                <span>
+                    {props.alt}
+                </span>
+            ) || (
+                <img
+                    alt={props.alt}
+                    src={props.src}
+                    srcSet={props.srcSet}
+                    onError={() => {
+                        setIsError(true);
+                    }}
+                />
+            ))}
+            {props.children}
         </span>
     )
 }
