c0b6a425b2e066cfaffdf84df04344bc78034029
nlichodeevskiy <nicklihorus4@gmail.com>
2021-05-27 09:53:52 +0700

    upd CalendarView styles, add CaptionElement

--- src/content/Calendar/CalendarView.tsx
+++ src/content/Calendar/CalendarView.tsx
@@ -2,90 +2,29 @@ import * as React from 'react';
 import {useCallback, useMemo} from 'react';
 import {useBem} from '@steroidsjs/core/hooks';
 import DayPicker from 'react-day-picker';
-import DropDownField from '@steroidsjs/core/ui/form/DropDownField';
 import {CaptionElementProps} from 'react-day-picker/types/Props';
 import {ICalendarViewProps} from '@steroidsjs/core/ui/content/Calendar/Calendar';
-import _upperFirst from 'lodash-es/upperFirst';
+import CaptionElement from './CaptionElement';
 
 import './CalendarView.scss';
-
-interface IYearMonthFormProps extends CaptionElementProps {
-    onChange: (value) => void
-    fromYear: Date,
-    toYear: Date,
-}
-
-function YearMonthForm(props: IYearMonthFormProps) {
-    const bem = useBem('CalendarView');
-
-    const {localeUtils, locale, fromYear, toYear, classNames, date} = props;
-    const currentMonth = date.getMonth();
-    const currentYear = date.getFullYear();
-
-    const handleYearChange = newYear => {
-        if (newYear !== currentYear) {
-            props.onChange(new Date(newYear, currentMonth));
-        }
-    };
-
-    const handleMonthChange = newMonth => {
-        if (newMonth !== currentMonth) {
-            props.onChange(new Date(currentYear, newMonth));
-        }
-    };
-
-    const months = useMemo(() => (
-        localeUtils.getMonths(locale).map((item, index) => ({
-            id: index,
-            label: _upperFirst(item),
-        }))
-    ), [localeUtils, locale]);
-
-    const years = useMemo(() => {
-        const result = [];
-        for (let i = fromYear.getFullYear(); i <= toYear.getFullYear(); i += 1) {
-            result.push(i);
-        }
-
-        return result;
-    }, [fromYear, toYear]);
-    return (
-        <div className={classNames.caption}>
-            <div className={bem.element('caption')}>
-                <div className={bem.element('caption-month')}>
-                    <DropDownField
-                        attribute='month'
-                        size='md'
-                        layout={false}
-                        noBorder
-                        items={months}
-                        onChange={(month) => handleMonthChange(month)}
-                        selectedIds={[currentMonth]}
-                    />
-                </div>
-                <div className={bem.element('caption-year')}>
-                    <DropDownField
-                        attribute='year'
-                        size='md'
-                        layout={false}
-                        className={bem.element('caption-month')}
-                        noBorder
-                        items={years}
-                        onChange={(year) => handleYearChange(year)}
-                        selectedIds={[currentYear]}
-                    />
-                </div>
-            </div>
-        </div>
-    );
-}
 
 export default function CalendarView(props: ICalendarViewProps) {
     const bem = useBem('CalendarView');
 
+    const {
+        month,
+        toYear,
+        fromYear,
+        onDaySelect,
+        selectedDates,
+        onMonthSelect,
+        toggleCaptionPanel,
+        isCaptionPanelVisible,
+    } = props;
+
     const {selectedDays, modifiers} = useMemo(() => {
-        const from = props.selectedDates[0];
-        const to = props.selectedDates[1];
+        const from = selectedDates[0];
+        const to = selectedDates[1];
 
         return {
             selectedDays: to
@@ -95,30 +34,41 @@ export default function CalendarView(props: ICalendarViewProps) {
                 ? { start: from, end: to }
                 : undefined,
         };
-    }, [props.selectedDates]);
-
-    const captionElement = useCallback(({classNames, date, localeUtils, locale}: CaptionElementProps) => (
-        <YearMonthForm
-            date={date}
-            localeUtils={localeUtils}
-            locale={locale}
-            classNames={classNames}
-            onChange={props.onMonthSelect}
-            fromYear={props.fromYear}
-            toYear={props.toYear}
-        />
-    ), [props.fromYear, props.onMonthSelect, props.toYear]);
+    }, [selectedDates]);
 
     return (
         <DayPicker
             className={bem.block({
-                ranged: props.selectedDates.length > 1,
+                ranged: selectedDates.length > 1,
             })}
-            captionElement={captionElement}
+            captionElement={useCallback(({classNames, date, localeUtils, locale}: CaptionElementProps) => (
+                <CaptionElement
+                    date={date}
+                    locale={locale}
+                    toYear={toYear}
+                    fromYear={fromYear}
+                    classNames={classNames}
+                    onChange={onMonthSelect}
+                    localeUtils={localeUtils}
+                    toggleCaptionPanel={toggleCaptionPanel}
+                    isCaptionPanelVisible={isCaptionPanelVisible}
+                />
+            ), [fromYear, isCaptionPanelVisible, onMonthSelect, toYear, toggleCaptionPanel])}
+            todayButton={isCaptionPanelVisible ? __('Close') : __('Today')}
+            onTodayButtonClick={(day) => {
+                if (isCaptionPanelVisible) {
+                    toggleCaptionPanel();
+                } else {
+                    onDaySelect(day);
+                }
+            }}
             selectedDays={selectedDays}
-            onDayClick={props.onDaySelect}
+            onDayClick={onDaySelect}
+            canChangeMonth={false}
             modifiers={modifiers}
-            month={props.month}
+            firstDayOfWeek={1}
+            month={month}
+            fixedWeeks
         />
     );
 }
---
+++ src/content/Calendar/CaptionElement.tsx
@@ -0,0 +1,194 @@
+import * as React from 'react';
+import {useMemo} from 'react';
+import {useBem} from '@steroidsjs/core/hooks';
+import Icon from '@steroidsjs/core/ui/icon/Icon';
+import {CaptionElementProps} from 'react-day-picker/types/Props';
+import {ICalendarViewProps} from '@steroidsjs/core/ui/content/Calendar/Calendar';
+import _upperFirst from 'lodash-es/upperFirst';
+
+import './CaptionElement.scss';
+
+interface ICaptionElementProps extends CaptionElementProps,
+    Pick<ICalendarViewProps, 'fromYear' | 'toYear' | 'isCaptionPanelVisible' | 'toggleCaptionPanel'>
+{
+    onChange: (value) => void
+}
+
+export default function CaptionElement(props: ICaptionElementProps) {
+    const bem = useBem('CaptionElement');
+    const {localeUtils, locale, fromYear, toYear, date, isCaptionPanelVisible, toggleCaptionPanel} = props;
+    const currentMonth = date.getMonth();
+    const currentYear = date.getFullYear();
+
+    const handleYearChange = newYear => {
+        if (newYear !== currentYear) {
+            props.onChange(new Date(newYear, currentMonth));
+        }
+    };
+
+    const handleMonthChange = newMonth => {
+        if (newMonth !== currentMonth) {
+            props.onChange(new Date(currentYear, newMonth));
+        }
+    };
+
+    const months = useMemo(() => (
+        localeUtils.getMonths(locale).map((item, index) => ({
+            id: index,
+            label: _upperFirst(item),
+        }))
+    ), [localeUtils, locale]);
+
+    const years = useMemo(() => {
+        const result = [];
+        for (let i = fromYear.getFullYear(); i <= toYear.getFullYear(); i += 1) {
+            result.push(i);
+        }
+        return result;
+    }, [fromYear, toYear]);
+
+    return (
+        <div className={bem(bem.block())}>
+            <div
+                className={bem.element('container')}
+            >
+                <Icon
+                    className={bem.element('button')}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleYearChange(currentYear - 1);
+                    }}
+                    name='angle-double-left'
+                />
+                <Icon
+                    className={bem.element('button', 'one-arrow')}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleMonthChange(currentMonth - 1);
+                    }}
+                    name='angle-left'
+                />
+                <div
+                    className={bem.element('content-container')}
+                    onKeyPress={e => {
+                        e.preventDefault();
+                        toggleCaptionPanel();
+                    }}
+                    onClick={e => {
+                        e.preventDefault();
+                        toggleCaptionPanel();
+                    }}
+                    role='button'
+                    tabIndex={0}
+                >
+                    <span className={bem.element('selected-month')}>
+                        {months.find(month => month.id === currentMonth).label}
+                    </span>
+                    <span className={bem.element('selected-year')}>
+                        {years.find(year => year === currentYear)}
+                    </span>
+                </div>
+                <Icon
+                    className={bem.element('button', 'one-arrow')}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleMonthChange(currentMonth + 1);
+                    }}
+                    name='angle-right'
+                />
+                <Icon
+                    className={bem.element('button')}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleYearChange(currentYear + 1);
+                    }}
+                    name='angle-double-right'
+                />
+            </div>
+            {isCaptionPanelVisible && (
+                <div className={bem.element('panel')}>
+                    <div className={bem.element('panel-header', 'months')}>
+                        {__('Month')}
+                    </div>
+                    <div className={bem.element('panel-header', 'years')}>
+                        {__('Year')}
+                    </div>
+                    <div className={bem.element('panel-column-months', 'first')}>
+                        <div className={bem.element('content-column')}>
+                            {months.slice(0, 6).map((value, index) => (
+                                <div
+                                    key={index}
+                                    className={bem.element('panel-cell', {
+                                        selected: value.id === currentMonth,
+                                    })}
+                                    onKeyPress={e => {
+                                        e.preventDefault();
+                                        handleMonthChange(value.id);
+                                    }}
+                                    onClick={e => {
+                                        e.preventDefault();
+                                        handleMonthChange(value.id);
+                                    }}
+                                    role='button'
+                                    tabIndex={0}
+                                >
+                                    <div className={bem.element('cell-value')}>
+                                        {value.label}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className={bem.element('panel-column-months', 'second')}>
+                        {months.slice(6, 12).map((value, index) => (
+                            <div
+                                key={index}
+                                className={bem.element('panel-cell', {
+                                    selected: value.id === currentMonth,
+                                })}
+                                onKeyPress={e => {
+                                    e.preventDefault();
+                                    handleMonthChange(value.id);
+                                }}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    handleMonthChange(value.id);
+                                }}
+                                role='button'
+                                tabIndex={0}
+                            >
+                                <div className={bem.element('cell-value')}>
+                                    {value.label}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className={bem.element('panel-column-years')}>
+                        {years.map((value, index) => (
+                            <div
+                                key={index}
+                                className={bem.element('panel-cell', {
+                                    selected: value === currentYear,
+                                })}
+                                onKeyPress={e => {
+                                    e.preventDefault();
+                                    handleYearChange(value);
+                                }}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    handleYearChange(value);
+                                }}
+                                role='button'
+                                tabIndex={0}
+                            >
+                                <div className={bem.element('cell-value')}>
+                                    {value}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
