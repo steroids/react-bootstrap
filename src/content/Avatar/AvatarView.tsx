69188a909e9811ff5a735bbdabe2d27706eaa298
nlichodeevskiy <nicklihorus4@gmail.com>
2021-06-01 23:53:12 +0700

    refactoring Avatar

--- src/content/Avatar/AvatarView.tsx
+++ src/content/Avatar/AvatarView.tsx
@@ -1,54 +1,54 @@
 import React from 'react';
 import { useBem } from '@steroidsjs/core/hooks';
-import { IAvatarViewProps } from '@steroidsjs/core/ui/content/Avatar/Avatar'
+import { IAvatarViewProps } from '@steroidsjs/core/ui/content/Avatar/Avatar';
 
 import './AvatarView.scss';
 
-
-export default function Avatar (props: IAvatarViewProps) {
+export default function Avatar(props: IAvatarViewProps) {
     const bem = useBem('AvatarView');
 
-    const customSize: React.CSSProperties =
-        typeof props.size === 'number'
+    const customSize: React.CSSProperties = typeof props.size === 'number'
         ? {
             width: props.size,
             height: props.size,
             lineHeight: `${props.size}px`,
-            fontSize:  props.size / 2,
-            }
+            fontSize: props.size / 2,
+        }
         : {};
-    
+
+    const renderImage = () => (
+        (props.isError && (
+            <span>
+                {props.formattedTitle}
+            </span>
+        )) || (
+            <img
+                alt={props.alt}
+                src={props.src}
+                title={props.title}
+                srcSet={props.srcSet}
+                onError={props.onError}
+            />
+        )
+    );
+
     return (
-        <div 
-        className={bem(bem.block({
-            [`${props.size}`]: typeof props.size === 'string',
-            [`${props.shape}`]: !!props.shape,
-            'has-image': !!props.src && !props.isError,
-            [`status`]: props.status === true,
-        }), props.className)}
+        <div
+            className={bem(bem.block({
+                size: props.size,
+                shape: props.shape,
+                'has-image': !!props.src && !props.isError,
+                'has-status': props.status,
+            }), props.className)}
         >
-            <span 
-                className={bem.element('body', {
-                   
-                })}
+            <span
+                className={bem.element('body')}
                 style={{
                     ...props.style,
                     ...customSize,
                 }}
             >
-                {(props.src && (props.isError && (
-                    <span>
-                        {props.formattedTitle}
-                    </span>
-                ) || (
-                    <img
-                        alt={props.alt}
-                        src={props.src}
-                        title={props.title}
-                        srcSet={props.srcSet}
-                        onError={props.onError}
-                    />
-                ))) || (
+                {(props.src && renderImage()) || (
                     <span>
                         {props.formattedTitle}
                     </span>
@@ -56,5 +56,5 @@ export default function Avatar (props: IAvatarViewProps) {
                 {props.children}
             </span>
         </div>
-    )
+    );
 }
