67720e45d7510d901569bac104c3f11c23745fcc
Nick <nicklihorus4@gmail.com>
2021-10-18 21:13:51 +0700

    #112 upd Modal (core & view), upd Modal demos

--- src/modal/Modal/ModalView.tsx
+++ src/modal/Modal/ModalView.tsx
@@ -1,54 +1,54 @@
 import * as React from 'react';
 import Modal from 'react-modal';
-
+import {useBem} from '@steroidsjs/core/hooks';
+import Controls from '@steroidsjs/core/ui/nav/Controls';
 import {IBemHocOutput} from '@steroidsjs/core/hoc/bem';
 import {IModalViewProps} from '@steroidsjs/core/ui/modal/Modal/Modal';
-import Controls from '@steroidsjs/core/ui/nav/Controls';
-import {useBem} from '@steroidsjs/core/hooks';
 
 export default function ModalView(props: IModalViewProps & IBemHocOutput) {
     const bem = useBem('ModalView');
     const overrideDefaultClasses = {
         base: bem.block('overlay'),
-        afterOpen: bem.block('overlay-after'),
-        beforeClose: bem.block('overlay-before'),
+        afterOpen: 'ModalView_overlay-after',
+        beforeClose: 'ModalView_overlay-before',
     };
     return (
-        <div>
-            <Modal
-                {...props}
-                isOpen={!props.isClosing}
-                closeTimeoutMS={200}
-                overlayClassName={overrideDefaultClasses}
-                className={bem.element('body', {size: props.size})}
-                bodyOpenClassName={bem.block('body-opened')}
-                ariaHideApp={false}
-            >
-                <div className={bem.element('header')}>
-                    <span className={bem.element('title')}>
-                        {props.title}
-                    </span>
-                    <button
-                        className={bem.element('close')}
-                        onClick={e => {
-                            e.preventDefault();
-                            props.onClose();
-                        }}
-                        aria-label={__('Закрыть')}
+        <Modal
+            {...props}
+            ariaHideApp={false}
+            bodyOpenClassName='ModalView_body-hide-scroll'
+            className={bem.element('body', {size: props.size})}
+            closeTimeoutMS={props.closeTimeoutMs}
+            isOpen={!props.isClosing}
+            onRequestClose={props.onClose}
+            overlayClassName={overrideDefaultClasses}
+            shouldCloseOnEsc={props.shouldCloseOnEsc}
+            shouldCloseOnOverlayClick={props.shouldCloseOnOverlayClick}
+        >
+            <div className={bem.element('header')}>
+                <span className={bem.element('title')}>
+                    {props.title}
+                </span>
+                <button
+                    className={bem.element('close')}
+                    onClick={e => {
+                        e.preventDefault();
+                        props.onClose();
+                    }}
+                    aria-label={__('Закрыть')}
+                />
+            </div>
+            <div className={bem.element('content')}>
+                {props.children}
+            </div>
+            {props.controls && (
+                <div className={bem.element('footer')}>
+                    <Controls
+                        className={bem.element('controls')}
+                        items={props.controls}
                     />
                 </div>
-                <div className={bem.element('content')}>
-                    {props.children}
-                </div>
-                {props.controls && (
-                    <div className={bem.element('footer')}>
-                        <Controls
-                            items={props.controls}
-                            className={bem.element('controls')}
-                        />
-                    </div>
-                )}
-            </Modal>
-        </div>
+            )}
+        </Modal>
     );
 }
