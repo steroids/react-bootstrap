19b131bc1e1c4450f0eab992ab206f7c4f5d2694
Asgard Pavlov <67398919+AsgardPavlov@users.noreply.github.com>
2021-10-28 16:28:14 +0700

    steroids/dev#137 - refactoring SliderField

--- src/form/SliderField/SliderFieldView.tsx
+++ src/form/SliderField/SliderFieldView.tsx
@@ -1,13 +1,57 @@
 import * as React from 'react';
-import Slider from 'rc-slider';
-
+import {useBem} from '@steroidsjs/core/hooks';
 import {IBemHocOutput} from '@steroidsjs/core/hoc/bem';
 import {ISliderFieldViewProps} from '@steroidsjs/core/ui/form/SliderField/SliderField';
-import {useBem} from '@steroidsjs/core/hooks';
+import Slider, {SliderTooltip, Handle, Range} from 'rc-slider';
 
 export default function SliderFieldView(props: ISliderFieldViewProps & IBemHocOutput) {
     const bem = useBem('SliderFieldView');
-    const SliderComponent: any = Slider; // TODO Fix error https://github.com/react-component/slider/issues/656
+
+    const createRangeWithTooltip = Slider.createSliderWithTooltip;
+    const createSliderWithTooltip = Slider.createSliderWithTooltip;
+    const RangeComponent: any = createRangeWithTooltip(Range);
+    const SliderComponent: any = createSliderWithTooltip(Slider);
+
+    const handle = (prevProps) => {
+        const { value } = prevProps;
+        return (
+            <SliderTooltip
+                prefixCls='rc-slider-tooltip'
+                overlay={`${value}`}
+                visible={props.tooltipIsVisible}
+                placement="top"
+            >
+                <Handle value={value} />
+            </SliderTooltip>
+        );
+    };
+
+    const commonProps = {
+        className: props.className,
+        min: props.min,
+        max: props.max,
+        step: props.step,
+        marks: props.marks,
+        onChange: props.onChange,
+        disabled: props.disabled,
+        tipFormatter: value => `${value + props.countFormat}`,
+        handle,
+    };
+
+    const RangeField = (
+        <RangeComponent
+            {...commonProps}
+            defaultValue={props.defaultValue ? props.defaultValue : [0, 10]}
+            areaDisabled
+            draggableTrack
+            pushable
+        />
+    );
+
+    const SliderField = (
+        <SliderComponent {...commonProps} defaultValue={5} />
+    );
+
     return (
         <div
             className={bem(
@@ -17,10 +61,7 @@ export default function SliderFieldView(props: ISliderFieldViewProps & IBemHocOu
                 props.className,
             )}
         >
-            <SliderComponent
-                {...props.slider}
-                className={bem.element('slider')}
-            />
+            {props.isRange ? RangeField : SliderField}
         </div>
     );
 }
