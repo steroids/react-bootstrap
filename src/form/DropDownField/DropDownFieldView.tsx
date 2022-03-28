7246613ee24d9c15575461fd8ae1695c83a2137a
Vladimir Kozhin <affka@affka.ru>
2022-03-28 12:55:03 +0700

    2.1.10 Исправил вьюшку DropDownField, чтобы лейблы брались из selectedItems, а не искались по списку items (их потому что может не быть)

--- src/form/DropDownField/DropDownFieldView.tsx
+++ src/form/DropDownField/DropDownFieldView.tsx
@@ -3,7 +3,6 @@ import {ReactText, useEffect, useRef} from 'react';
 
 import {IDropDownFieldViewProps} from '@steroidsjs/core/ui/form/DropDownField/DropDownField';
 import {useBem} from '@steroidsjs/core/hooks';
-import {getLinearItems} from '@steroidsjs/core/hooks/useDataSelect';
 
 export default function DropDownFieldView(props: IDropDownFieldViewProps) {
     const inputRef = useRef(null);
@@ -69,57 +68,53 @@ export default function DropDownFieldView(props: IDropDownFieldViewProps) {
                 tabIndex={0}
                 role='button'
             >
-                {props.selectedIds
-                    .map(id => getLinearItems(props.items, props.groupAttribute).find(item => item[props.primaryKey] === id))
-                    .filter(Boolean)
-                    .map(item => (
-                        props.multiple
-                            ? (
+                {props.selectedItems.map(item => (
+                    props.multiple
+                        ? (
+                            <span
+                                key={String(item.id)}
+                                className={bem.element('selected-item-multiple')}
+                            >
+                                {item.label}
                                 <span
-                                    key={String(item.id)}
-                                    className={bem.element('selected-item-multiple')}
+                                    className={bem.element('selected-item-multiple-remove')}
+                                    onClick={e => {
+                                        e.preventDefault();
+                                        e.stopPropagation();
+                                        props.onItemRemove(item[props.primaryKey]);
+                                    }}
+                                    onKeyPress={e => {
+                                        e.preventDefault();
+                                        e.stopPropagation();
+                                        props.onItemRemove(item[props.primaryKey]);
+                                    }}
+                                    tabIndex={0}
+                                    role='button'
                                 >
-                                    {item.label}
-                                    <span
-                                        className={bem.element('selected-item-multiple-remove')}
-                                        onClick={e => {
-                                            e.preventDefault();
-                                            e.stopPropagation();
-                                            props.onItemRemove(item[props.primaryKey]);
-                                        }}
-                                        onKeyPress={e => {
-                                            e.preventDefault();
-                                            e.stopPropagation();
-                                            props.onItemRemove(item[props.primaryKey]);
-                                        }}
-                                        tabIndex={0}
-                                        role='button'
+                                    <svg
+                                        viewBox="64 64 896 896"
+                                        className=""
+                                        width="1em"
+                                        height="1em"
                                     >
-                                        <svg
-                                            viewBox="64 64 896 896"
-                                            className=""
-                                            width="1em"
-                                            height="1em"
-                                        >
-                                            <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3
-                                             5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4
-                                             512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3
-                                              3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"
-                                            />
-                                        </svg>
-                                    </span>
+                                        <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3
+                                         5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4
+                                         512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3
+                                          3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"
+                                        />
+                                    </svg>
                                 </span>
-                            )
-                            : (
-                                <span
-                                    key={item.id as ReactText}
-                                    className={bem.element('selected-item')}
-                                >
-                                    {item.label}
-                                </span>
-                            )
-                    ))
-                }
+                            </span>
+                        )
+                        : (
+                            <span
+                                key={item.id as ReactText}
+                                className={bem.element('selected-item')}
+                            >
+                                {item.label}
+                            </span>
+                        )
+                ))}
             </div>
             {props.showReset && props.selectedIds.length > 0 && (
                 <button
