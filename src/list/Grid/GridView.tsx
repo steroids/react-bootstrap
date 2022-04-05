70fb04eaf951b976d69369996d708a95b62426d4
Vladimir Kozhin <affka@affka.ru>
2022-04-05 09:34:42 +0700

    2.1.12 apply size for form in Grid

--- src/list/Grid/GridView.tsx
+++ src/list/Grid/GridView.tsx
@@ -33,6 +33,7 @@ export default function GridView(props: IGridViewProps) {
                         {column.attribute && fields[column.attribute] && (
                             <Field
                                 layout='inline'
+                                size={props.searchForm?.size}
                                 {...fields[column.attribute]}
                             />
                         )}
