f10ffed260d164f8526af5590be9c42f02d3f476
Dmitriy <kraxtv471365@gmail.com>
2023-02-13 14:04:43 +0700

    Alert full refactoring styles

--- src/content/Alert/AlertView.tsx
+++ src/content/Alert/AlertView.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useBem } from '@steroidsjs/core/hooks';
+import {useBem} from '@steroidsjs/core/hooks';
 import Icon from '@steroidsjs/core/ui/icon/Icon';
-import { IAlertViewProps } from '@steroidsjs/core/ui/content/Alert/Alert';
+import {IAlertViewProps} from '@steroidsjs/core/ui/content/Alert/Alert';
 
 export default function Alert(props: IAlertViewProps) {
     const bem = useBem('AlertView');
@@ -11,38 +11,55 @@ export default function Alert(props: IAlertViewProps) {
                 className={bem(
                     bem.block({
                         [props.type]: !!props.type,
-                        'has-description': !!props.description,
                         'close-animation': !props.isVisible,
                     }), props.className,
                 )}
                 style={props.style}
             >
-                {props.showIcon && (
-                    <Icon
-                        name={props.type}
-                        className={bem.element('icon', {
-                            [props.type]: !!props.type,
-                        })}
-                    />
-                )}
-                {typeof props.showIcon === 'string' && (
-                    <Icon
-                        name={props.showIcon}
-                        className={bem.element('icon')}
-                    />
-                )}
                 <div className={bem.element('content-wrapper')}>
                     <div className={bem.element('content')}>
-                        {props.message && (
-                            <div className={bem.element('message')}>
-                                {props.message}
-                            </div>
-                        )}
-                        {props.description && (
-                            <div className={bem.element('description')}>
-                                {props.description}
-                            </div>
-                        )}
+                        <div className={bem.element('icon-section')}>
+                            {props.showIcon && (
+                                <Icon
+                                    name={props.type}
+                                    className={bem.element('icon', {
+                                        [props.type]: !!props.type,
+                                    })}
+                                />
+                            )}
+                            {typeof props.showIcon === 'string' && (
+                                <Icon
+                                    name={props.showIcon}
+                                    className={bem.element('icon')}
+                                />
+                            )}
+                        </div>
+                        <div className={bem.element('text-section')}>
+                            {props.message && (
+                                <div className={bem.element('message')}>
+                                    {props.message}
+                                </div>
+                            )}
+                            {props.description && (
+                                <div className={bem.element('description')}>
+                                    {props.description}
+                                </div>
+                            )}
+                        </div>
+                        <div className={bem.element('close-section')}>
+                            {props.showClose && (
+                                <Icon
+                                    className={bem.element('icon-close', {
+                                        large: !!props.description,
+                                    })}
+                                    name='close'
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        props.onClose();
+                                    }}
+                                />
+                            )}
+                        </div>
                     </div>
                     {props.action && (
                         <div className={bem.element('action')}>
@@ -50,18 +67,6 @@ export default function Alert(props: IAlertViewProps) {
                         </div>
                     )}
                 </div>
-                {props.showClose && (
-                    <Icon
-                        className={bem.element('icon-close', {
-                            large: !!props.description,
-                        })}
-                        name='times'
-                        onClick={(e) => {
-                            e.preventDefault();
-                            props.onClose();
-                        }}
-                    />
-                )}
             </div>
         )
     );
