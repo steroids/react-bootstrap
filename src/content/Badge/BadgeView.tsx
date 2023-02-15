5dff9260e83d8ce335a6d4658e4aa26c606ae8ea
RaayNoff <kraxtv471365@gmail.com>
2023-02-15 16:09:16 +0700

    Minor changes

--- src/content/Badge/BadgeView.tsx
+++ src/content/Badge/BadgeView.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import useBem from '@steroidsjs/core/hooks/useBem';
 import {IBadgeViewProps} from '@steroidsjs/core/ui/content/Badge/Badge';
 
 import './BadgeView.scss';
+import Icon from '@steroidsjs/core/ui/icon/Icon';
 
 export default function BadgeView(props: IBadgeViewProps) {
     const bem = useBem('BadgeView');
 
     return (
         props.isExist && (
-            <div className={bem(
-                bem.block({
-                    [props.type]: !!props.type,
-                    'close-animation': !props.isVisible,
-                }), props.className,
-            )}
+            <div
+                className={bem(
+                    bem.block({
+                        [props.type]: !!props.type,
+                        'close-animation': !props.isVisible,
+                    }), props.className,
+                )}
                 style={props.style}
             >
                 <div className={bem.element('content-wrapper')}>
-                    <div className={bem.element('content')}></div>
+                    <span className={bem.element('content')}>
+                        {props.message}
+                    </span>
+                    {props.showClose && (
+                        <Icon
+                            name='close'
+                            onClick={props.onClose}
+                        />
+                    )}
+                    {props.hasChip
+                        && (
+                            <div className={bem.element('chip')}>
+                                {props.chipContent}
+                            </div>
+                        )}
                 </div>
             </div>
         )
