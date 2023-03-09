fba23bc57a7d738d574acd293dde23e17bbeaad1
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-03-09 16:21:03 +0800

    Rename and fix props

--- src/typography/Text/TextView.tsx
+++ src/typography/Text/TextView.tsx
@@ -2,17 +2,23 @@ import * as React from 'react';
 import {useBem} from '@steroidsjs/core/hooks';
 import {ITextViewProps} from '@steroidsjs/core/ui/typography/Text/Text';
 
+const TYPE_MAPPING = {
+    body: 'p',
+    span: 'span',
+    boldSpan: 'span',
+}
+
 export default function TextView(props: ITextViewProps) {
     const bem = useBem('TextView');
-    const component = props.component || props.templateMapping[props.template] || 'p';
+    const tag = props.tag || TYPE_MAPPING[props.type] || 'p';
 
     return (
         React.createElement(
-            component,
+            tag,
             {
                 className: bem(
                     bem.block({
-                        template: props.template,
+                        type: props.type,
                         color: props.color,
                     }),
                     props.className,
--- src/typography/Title/TitleView.tsx
+++ src/typography/Title/TitleView.tsx
@@ -2,17 +2,27 @@ import * as React from 'react';
 import {useBem} from '@steroidsjs/core/hooks';
 import {ITitleViewProps} from '@steroidsjs/core/ui/typography/Title/Title';
 
+const TYPE_MAPPING = {
+    h1: 'h1',
+    h2: 'h2',
+    h3: 'h3',
+    h4: 'h4',
+    h5: 'h5',
+    h6: 'h6',
+    subtitle: 'h6',
+}
+
 export default function TitleView(props: ITitleViewProps) {
     const bem = useBem('TitleView');
-    const component = props.component || props.templateMapping[props.template] || 'h2';
+    const tag = props.tag || TYPE_MAPPING[props.type] || 'h2';
 
     return (
         React.createElement(
-            component,
+            tag,
             {
                 className: bem(
                     bem.block({
-                        template: props.template,
+                        type: props.type,
                         color: props.color,
                     }),
                     props.className,
