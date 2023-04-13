6a4a3d80d21126fca0646d915da38f996d0c5553
Dmitriy Babiy <dimascelecto@gmail.com>
2023-04-14 02:37:26 +0700

    Transferring renderMenuItems to the Menu.tsx

--- src/content/Menu/MenuView.tsx
+++ src/content/Menu/MenuView.tsx
@@ -3,21 +3,9 @@ import {IMenuViewProps} from '@steroidsjs/core/ui/content/Menu/Menu';
 import {DropDown, Icon} from '@steroidsjs/core/ui/content';
 import {useBem} from '@steroidsjs/core/hooks';
 import renderIcon from '../../utils/renderIcon';
-import MenuItemView from './MenuItemView';
 
 export default function MenuView(props: IMenuViewProps) {
     const bem = useBem('MenuView');
-
-    const renderMenuItems = React.useCallback(() => (
-        <>
-            {props.items.map((item, index) => (
-                <MenuItemView
-                    key={index}
-                    {...item}
-                />
-            ))}
-        </>
-    ), [props.items]);
 
     const {icon, items, className, children, ...dropDownProps} = props;
 
@@ -25,7 +13,7 @@ export default function MenuView(props: IMenuViewProps) {
         <DropDown
             {...dropDownProps}
             className={bem(bem.block(), props.className)}
-            content={renderMenuItems}
+            content={props.renderMenuItems}
         >
             <span className={bem.element('button')}>
                 {props.icon
