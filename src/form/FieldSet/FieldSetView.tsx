9771f0c0ee55d193e41af00dfa59ddc08d8cdf76
Dmitriy Babiy <dimascelecto@gmail.com>
2023-04-25 12:26:48 +0700

    Add styles for FieldSet

--- src/form/FieldSet/FieldSetView.tsx
+++ src/form/FieldSet/FieldSetView.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import {IFieldSetViewProps} from '@steroidsjs/core/ui/form/FieldSet/FieldSet';
+import {useBem} from '@steroidsjs/core/hooks';
 
 export default function FieldSetView(props: IFieldSetViewProps) {
+    const bem = useBem('FieldSetView');
     return (
-        <fieldset className={props.className}>
-            <legend>{props.label}</legend>
+        <fieldset className={bem(bem.block(), props.className)}>
+            <legend className={bem.element('legend')}>{props.label}</legend>
             {props.children}
         </fieldset>
     );
