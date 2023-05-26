76bc16fe72531cd84683ba95ddeefa293d025b29
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-05-26 13:33:50 +0800

    Fix

--- src/list/FlexGrid/FlexGridView.tsx
+++ src/list/FlexGrid/FlexGridView.tsx
@@ -1,27 +1,21 @@
 import * as React from 'react';
 
-import {useBem, useScreen} from '@steroidsjs/core/hooks';
-import {IFlexGridViewProps} from '@steroidsjs/core/ui/list/FlexGrid';
+import {useBem} from '@steroidsjs/core/hooks';
+import {IFlexGridViewProps} from '@steroidsjs/core/ui/list/FlexGrid/FlexGrid';
 
 export default function FlexGridView(props: IFlexGridViewProps) {
     const bem = useBem('FlexGridView');
-    const {isDesktop, isTablet, isPhone} = useScreen();
+    const halfColGap = props.colGap / 2;
+    const hasItems = !!props.items?.length;
 
-    const halfColGapLg = props.lg?.colGap / 2;
-    const halfColGapMd = props.md?.colGap / 2;
-    const halfColGapSm = props.sm?.colGap / 2;
-
-    const halfColGap = props.colGap / 2
-        || (isDesktop && halfColGapLg)
-        || (isTablet && halfColGapMd)
-        || (isPhone && halfColGapSm);
-
-    const rowGap = props.rowGap
-        || (isDesktop && props.lg?.rowGap)
-        || (isTablet && props.md?.rowGap)
-        || (isPhone && props.sm?.rowGap);
-
-    console.log(isDesktop, isTablet, isPhone);
+    const colGapStyle = hasItems
+        ? {
+            marginRight: -halfColGap,
+            marginLeft: -halfColGap,
+        }
+        : {
+            columnGap: props.colGap,
+        };
 
     return (
         <div
@@ -36,20 +30,28 @@ export default function FlexGridView(props: IFlexGridViewProps) {
             )}
             style={{
                 ...props.style,
-                marginRight: -halfColGap,
-                marginLeft: -halfColGap,
-                rowGap,
+                ...colGapStyle,
+                rowGap: props.rowGap,
+
             }}
         >
-            {!!props.items?.length && props.items.map((item, index) => (
+            {hasItems && props.items.map((item, index) => (
                 <div
                     key={index}
-                    className={bem.element('item', {
-                        col: item.col,
-                    })}
+                    className={bem(
+                        bem.element('item', {
+                            col: item.col,
+                            lg: item.lg,
+                            md: item.md,
+                            sm: item.sm,
+                            offset: item.offset,
+                        }),
+                        props.itemClassName,
+                    )}
                     style={{
                         paddingRight: halfColGap,
                         paddingLeft: halfColGap,
+                        order: item.order,
                     }}
                 >
                     {item.content}
