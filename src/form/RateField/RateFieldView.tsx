aa28d8546f706a2e4a683b19f063142103fcb42e
Dmitriy <kraxtv471365@gmail.com>
2023-06-14 09:53:27 +0700

    RateField

--- src/form/RateField/RateFieldView.tsx
+++ src/form/RateField/RateFieldView.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import * as React from 'react';
 import {useBem} from '@steroidsjs/core/hooks';
 import Icon from '@steroidsjs/core/ui/content/Icon';
@@ -7,33 +8,40 @@ export default function RateFieldView(props: IRateFieldViewProps) {
     const bem = useBem('RateFieldView');
     return (
         <div
-            className={bem(bem.block(), props.className)}
+            className={bem(bem.block({
+                size: props.size,
+                disabled: props.disabled,
+            }), props.className)}
+            style={props.style}
         >
-            {props.items.map((item, index) => (
-                <div
-                    key={index}
-                    className={bem.element('rate-item', {
-                        'is-full': item.value <= props.inputProps.value,
-                    })}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        props.onItemClick(item);
-                    }}
-                >
-                    <span className={bem.element('rate-value-first')}>
-                        <Icon
-                            className={bem.element('rate-icon')}
-                            name='star'
-                        />
-                    </span>
-                    <span className={bem.element('rate-value-second')}>
-                        <Icon
-                            className={bem.element('rate-icon')}
-                            name='star'
-                        />
-                    </span>
-                </div>
-            ))}
+            <ul className={bem.element('rate-list')}>
+                {props.items.map((item, index) => (
+                    <div
+                        key={index}
+                        className={bem.element('rate-item', {
+                            'is-full': item.value <= props.inputProps.value,
+                        })}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            props.onItemClick(item);
+                        }}
+                    >
+                        <span className={bem.element('rate-value-first')}>
+                            <Icon
+                                className={bem.element('rate-icon')}
+                                name='star'
+                            />
+                        </span>
+                        <span className={bem.element('rate-value-second')}>
+                            <Icon
+                                className={bem.element('rate-icon')}
+                                name='star'
+                            />
+                        </span>
+                    </div>
+                ))}
+            </ul>
+            {props.badge && <span className={bem.element('badge')}>{props.badge?.title}</span>}
         </div>
     );
 }
