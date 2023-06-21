44947fb1ab5500ba5019d9021e8c82e34b8229e2
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-06-21 17:48:50 +0800

    Update steroids

--- src/form/ReCaptchaField/ReCaptchaFieldView.tsx
+++ src/form/ReCaptchaField/ReCaptchaFieldView.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 
 import {IReCaptchaFieldViewProps} from '@steroidsjs/core/ui/form/ReCaptchaField/ReCaptchaField';
-import {useBem} from '@steroidsjs/core/hooks';
+import {useBem, useTheme} from '@steroidsjs/core/hooks';
 import ReCAPTCHA from 'react-google-recaptcha';
 
 export default function ReCaptchaFieldView(props: IReCaptchaFieldViewProps) {
     const bem = useBem('ReCaptchaFieldView');
+    const {theme} = useTheme();
 
     return (
         <div
@@ -17,6 +18,7 @@ export default function ReCaptchaFieldView(props: IReCaptchaFieldViewProps) {
         >
             <ReCAPTCHA
                 {...props.recaptchaOptions}
+                theme={theme}
             />
         </div>
     );
