c0f21b89836f9726fe5289433917de5f0565a80c
Dmitriy <kraxtv471365@gmail.com>
2023-08-30 10:49:46 +0700

    Style changes for adaptive in steroids-site

--- src/layout/Notifications/NotificationsView.tsx
+++ src/layout/Notifications/NotificationsView.tsx
@@ -3,6 +3,9 @@ import * as React from 'react';
 import {INotificationsViewProps} from '@steroidsjs/core/ui/layout/Notifications/Notifications';
 import {useBem} from '@steroidsjs/core/hooks';
 
+
+
+
 export default function NotificationsView(props: INotificationsViewProps) {
     const bem = useBem('NotificationsView');
     return (
