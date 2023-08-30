6f0880bbe13b376e43c32cf2593a1605befd8b57
Dmitriy <kraxtv471365@gmail.com>
2023-08-30 10:55:02 +0700

    Lint fixes

--- src/layout/Notifications/NotificationsView.tsx
+++ src/layout/Notifications/NotificationsView.tsx
@@ -3,9 +3,6 @@ import * as React from 'react';
 import {INotificationsViewProps} from '@steroidsjs/core/ui/layout/Notifications/Notifications';
 import {useBem} from '@steroidsjs/core/hooks';
 
-
-
-
 export default function NotificationsView(props: INotificationsViewProps) {
     const bem = useBem('NotificationsView');
     return (
