0f1b5dfb8b79735699bae5412ff5598ccfe4d795
Konstantin Smirnov <smirnov.konstantiin@gmail.com>
2023-11-01 04:03:05 +0700

    Add scroll handler

--- src/content/Chat/ChatView.tsx
+++ src/content/Chat/ChatView.tsx
@@ -1,5 +1,4 @@
-import React, {useCallback, useRef} from 'react';
-import {useUpdateEffect} from 'react-use';
+import React, {useCallback, useEffect, useRef} from 'react';
 import useBem from '@steroidsjs/core/hooks/useBem';
 import {IChatViewProps} from '@steroidsjs/core/ui/content/Chat/Chat';
 
@@ -9,25 +8,49 @@ import BubblesDateGroup from './views/BubblesDateGroup';
 export default function ChatView(props: IChatViewProps) {
     const bem = useBem('ChatView');
 
-    const endOfMessagesRef = useRef(null);
+    const wrapperRef = useRef<HTMLDivElement | null>(null);
+    const contentRef = useRef<HTMLDivElement | null>(null);
 
-    useUpdateEffect(() => {
-        endOfMessagesRef?.current.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'start'});
-    }, [props.groupedMessagesByDates]);
+    const prevContentHeightRef = useRef(null);
+
+    useEffect(() => {
+        const wrapperHeight = wrapperRef.current.clientHeight;
+        const contentHeight = contentRef.current.clientHeight;
+        const wrapperScrollTop = wrapperRef.current.scrollTop;
+
+        if (!prevContentHeightRef.current || wrapperScrollTop === prevContentHeightRef.current - wrapperHeight) {
+            wrapperRef.current.scrollTo({
+                top: contentHeight - wrapperHeight,
+                left: 0,
+                behavior: prevContentHeightRef.current ? 'smooth' : 'auto',
+            });
+        }
+
+        prevContentHeightRef.current = contentHeight;
+    });
 
     const renderChatScreen = useCallback(() => (
-        <div className={bem.element('screen')}>
-            <div className={bem.element('content')}>
-                {Object.entries(props.groupedMessagesByDates)
-                    .map(([date, groupedMessages]) => (
-                        <BubblesDateGroup
-                            key={date}
-                            date={date}
-                            groupedMessages={groupedMessages}
-                            currentUser={props.currentUser}
-                        />
-                    ))}
-                <div ref={endOfMessagesRef} />
+        <div
+            className={bem.element('screen')}
+        >
+            <div
+                className={bem.element('wrapper')}
+                ref={wrapperRef}
+            >
+                <div
+                    className={bem.element('content')}
+                    ref={contentRef}
+                >
+                    {Object.entries(props.groupedMessagesByDates)
+                        .map(([date, groupedMessages]) => (
+                            <BubblesDateGroup
+                                key={date}
+                                date={date}
+                                groupedMessages={groupedMessages}
+                                currentUser={props.currentUser}
+                            />
+                        ))}
+                </div>
             </div>
         </div>
     ), [bem, props.groupedMessagesByDates, props.currentUser]);
